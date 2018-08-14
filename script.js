40a23c5fb02597c58fc3edc047babd7ef7f20a16
Devid <rinzler91@mail.ru>
2018-08-14 15:37:09 +0600

    Date Picker , Bootstrap select

---
+++ script.js
@@ -0,0 +1,15 @@
+$('.select').selectpicker({
+    style: 'btn-info',
+    size: 4
+});
+
+$('.page-ico--datepicker__input').datepicker({
+    autoclose: true,
+    format: 'd MM yyyy'
+});
+
+//  Timepicker
+
+$('.timepicker').timepicker({
+    'timeFormat': 'h:i A'
+});
